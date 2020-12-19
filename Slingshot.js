6a8f05938efba67537d1bfcdd7e4181794b319b9
Jashy8 <grewaljashn@gmail.com>
2020-12-18 20:37:43 -0500

    Slingshot Finished

--- Slingshot.js
+++ Slingshot.js
@@ -6,19 +6,36 @@ class Slingshot {
             'stiffness': 0.7,
             'length': 0.5
         }
+        this.sling1 = loadImage("sprites/sling1.png");
+        this.sling2 = loadImage("sprites/sling2.png");
+        this.sling3 = loadImage("sprites/sling3.png");
         this.pointB = pointB;
         this.slingshot = Constraint.create(options);
         World.add(world,this.slingshot);
     }
     display(){
-       
+       image(this.sling1, 200, 23);
+       image(this.sling2, 170, 23);
         if (this.slingshot.bodyA){
             var pointA=this.slingshot.bodyA.position;
             var pointB=this.pointB; 
             push();
-            strokeWeight(5);
-            stroke("red");
-            line(pointA.x,pointA.y,pointB.x,pointB.y);
+            stroke(48,22,8);
+            if(pointA.x<220){
+                strokeWeight(7);
+                line(pointA.x-20,pointA.y,pointB.x-10,pointB.y);
+                line(pointA.x-20,pointA.y,pointB.x+30,pointB.y-3);
+                image(this.sling3,pointA.x-30,pointA.y-10, 15, 30);
+            }
+            else{
+                strokeWeight(7);
+                line(pointA.x+25,pointA.y,pointB.x-10,pointB.y);
+                line(pointA.x+25,pointA.y,pointB.x+30,pointB.y-3);
+                image(this.sling3,pointA.x+25,pointA.y-10, 15, 30);
+            }
+
+
+
             pop();
         }
         
